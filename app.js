1a0663636e56078cf50d1a213e46ba4c7ff869a7
Ashish Patel <Ashish.Patel@rishabh.com>
2017-02-22 11:50:28 +0530

    First commit

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
   config = require('./config/config'),
   passport = require('passport'),
   logger = require('./config/log'),
+<<<<<<< b2af2dd959a144bde511965f41943db410b2b2bb
+=======
+  socketio = require('socket.io'),
+>>>>>>> First commit
   requireWalk = require('./www/utils/requireWalk').requireWalk;
 
 // Expressjs setup
@@ -23,7 +27,21 @@ var requireRoutes = requireWalk(config.get('root') + '/www/routes');
 requireRoutes(config, app, passport);
 
 // Start web server
+<<<<<<< b2af2dd959a144bde511965f41943db410b2b2bb
 http.createServer(app).listen(app.get('port'), function () {
   msg = 'Express server listening on port ' + app.get('port') + ' in ' + env + ' mode';
   logger.info(msg);
 });
+=======
+var server = http.createServer(app).listen(app.get('port'), function () {
+  msg = 'Express server listening on port ' + app.get('port') + ' in ' + env + ' mode';
+  logger.info(msg);
+});
+
+socketio.listen(server).on('connection', function (socket) {
+    socket.on('message', function (msg) {
+        console.log('Message Received: ', msg);
+        socket.broadcast.emit('message', msg);
+    });
+});
+>>>>>>> First commit
--- dist/bower_components/angular-scenario/jstd-scenario-adapter-config.js
+++ dist/bower_components/angular-scenario/jstd-scenario-adapter-config.js
@@ -1,4 +1,8 @@
+<<<<<<< b2af2dd959a144bde511965f41943db410b2b2bb
 /**
+=======
+/**
+>>>>>>> First commit
  * Configuration for jstd scenario adapter 
  */
 var jstdScenarioAdapter = {
